2a42fbee9ff0ab1bc646f0e11b3f47f4560b44fd
benoit <steve.benoit@colostate.edu>
2024-08-15 19:54:25 -0600

    Update gradle dependencies, commons library

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,15 @@ repositories {
 }
 
 dependencies {
-    implementation("com.ibm.informix:jdbc:4.50.10")
-    implementation("com.oracle.database.jdbc:ojdbc11:23.3.0.23.09")
-    implementation("io.undertow:undertow-core:2.3.12.Final")
-    implementation("io.undertow:undertow-servlet:2.3.12.Final")
-    implementation("io.undertow:undertow-websockets-jsr:2.3.12.Final")
+    implementation("com.ibm.informix:jdbc:4.50.11")
+    implementation("com.oracle.database.jdbc:ojdbc11:23.4.0.24.05")
+    implementation("io.undertow:undertow-core:2.3.15.Final")
+    implementation("io.undertow:undertow-servlet:2.3.15.Final")
+    implementation("io.undertow:undertow-websockets-jsr:2.3.15.Final")
 
     implementation(files("lib/mathops_commons.jar"))
 
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
